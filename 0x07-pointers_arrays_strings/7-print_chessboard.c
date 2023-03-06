37412922be6c7d3c6434a5e033e48935f2b7b359
Didiamaku <didiamaku@gmail.com>
2023-03-06 10:27:32 -0800

    more pointers

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,7 @@
 /**
  * print_chessboard - a function that prints the chessboard.
  * @a: array containing board elements
- * returns nothing
+ * Return: Always 0 (Success)
  */
 
 void print_chessboard(char (*a)[8])
