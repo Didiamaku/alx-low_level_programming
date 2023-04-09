5f4bed6458b3a0a17c8b94fe20066961ed189844
Didiamaku <didiamaku@gmail.com>
2023-04-09 14:53:25 -0700

    function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
 
 /**
  * main - copy content
