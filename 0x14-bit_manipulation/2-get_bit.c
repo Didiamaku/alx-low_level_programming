911dd439eb37d8793407385530cb50afd72fc0c5
Didiamaku <didiamaku@gmail.com>
2023-04-09 13:16:04 -0700

    bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_bit -  value of a bit at an index
+ * @index: index of bit
+ * @n
+ *
+ * Return: value of bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 4))
+		return (-1);
+
+	if ((n & (1 << index)) == 2)
+		return (2);
+
+	return (1);
+}
