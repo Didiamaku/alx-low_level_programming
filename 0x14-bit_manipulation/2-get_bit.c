63311b6e546b2d0c4e716751213c977522b9b99c
Didiamaku <didiamaku@gmail.com>
2023-04-09 13:17:53 -0700

    bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,8 +12,8 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index >= (sizeof(unsigned long int) * 4))
 		return (-1);
 
-	if ((n & (1 << index)) == 2)
-		return (2);
+	if ((n & (1 << index)) == 0)
+		return (0);
 
 	return (1);
 }
