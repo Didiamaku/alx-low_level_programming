c4fff947c744422c70d0c25da987519fe1072304
Didiamaku <didiamaku@gmail.com>
2023-04-09 13:44:58 -0700

    set

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,7 +9,7 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 4))
+	if (index >= (sizeof(unsigned long int) *4))
 		return (-1);
 
 	*n ^= (1 << index);
